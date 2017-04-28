9cced0857e1cacff7a34b5e3d30a854773f60499
StevenLiao <slamslamslam12411@gmail.com>
2017-04-28 16:37:59 +0800

    4/28-SQL

--- app/src/main/java/com/example/stevenliao/sqlitetest/MainActivity.java
+++ app/src/main/java/com/example/stevenliao/sqlitetest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.stevenliao.sqlitetest;
 
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,15 +23,26 @@ public class MainActivity extends AppCompatActivity {
         edNumbewr = (EditText) findViewById(R.id.editText2);
         helper = new MyDBHelper(this, "expense.db", null, 1);
         SqlBtn = (Button) findViewById(R.id.button);
-
+        MyDBHelper helper = MyDBHelper.getInstance(this);
 
         SqlBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                add();
+                Log.d("Create db", "Success");
 
             }
         });
     }
 
+    public void add () {
+        String cName = edName.getText().toString();
+        String cNumber = edNumbewr.getText().toString();
+        ContentValues values = new ContentValues();
+        values.put("_id", 22);
+        values.put("cdate", 1234);
+        long id = helper.getWritableDatabase().insert("exp",null,values);
+        Log.d("ADD:", id+"");
+    }
 
 }
--- app/src/main/java/com/example/stevenliao/sqlitetest/MyDBHelper.java
+++ app/src/main/java/com/example/stevenliao/sqlitetest/MyDBHelper.java
@@ -1,5 +1,6 @@
 package com.example.stevenliao.sqlitetest;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,6 +10,14 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class MyDBHelper extends SQLiteOpenHelper {
+    private static MyDBHelper instance = null;
+
+    public static MyDBHelper getInstance(Context ctx) {
+        if (instance == null) {
+            instance = new MyDBHelper(ctx, "expense.db", null, 1);
+        }
+        return instance;
+    }
 
     public MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -16,9 +25,20 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE main.exp" +
-                    "(name VARCHAR(10) PRIMARY KEY NOT NULL , " +
-                    "number INTEGER)");
+        db.execSQL("CREATE  TABLE main.exp " +
+                "(_id INTEGER PRIMARY KEY  NOT NULL, " +
+                "cdate INTEGER, " +
+                "info VARCHAR, " +
+                "amount INTEGER)");
+        db.execSQL("CREATE TABLE infos " +
+                "(_id INTEGER PRIMARY KEY  NOT NULL , " +
+                "info VARCHAR NOT NULL)");
+        String[] infos = {"Breakfast", "Bread", "Lunch", "Parking", "Bill"};
+        for (String info : infos) {
+            ContentValues values = new ContentValues();
+            values.put("info", info);
+            db.insert("infos", null, values);
+        }
     }
 
     @Override
